f2624f160901ba7f9ba3caca501940ac8d029ff9
Nicolás Castro <npcastrol@gmail.com>
2017-06-12 13:10:13 -0400

    Optimize mxnet script

--- 5. mxnet/hr_example.py
+++ 5. mxnet/hr_example.py
@@ -1,50 +1,93 @@
 import mxnet as mx
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix
+
+
 
 # Read data and basic statistics
 hr_data = pd.read_csv("data/hr.csv")
-hr_data.head()
-hr_data.info()
-hr_data.describe()
 
-# Convert some variables to factors
-hr_data[['Work_accident', 'promotion_last_5years']] = \
-    hr_data[['Work_accident', 'promotion_last_5years']].astype(object)
+print hr_data.head()
+print hr_data.info()
+print hr_data.describe()
 
+
+#Convert some features to numeric
+sales_unique = hr_data['sales'].unique().tolist()
+sales_encoder = preprocessing.LabelEncoder()
+sales_encoder.fit(sales_unique)
+
+hr_data['sales'] = sales_encoder.transform(hr_data['sales'])
+
+salary_unique = hr_data['salary'].unique().tolist()
+salary_encoder = preprocessing.LabelEncoder()
+salary_encoder.fit(salary_unique)
+
+hr_data['salary'] = salary_encoder.transform(hr_data['salary'])
+
+
+# Separate data in training and testing
+X = hr_data.drop(['left'], axis=1)
+y = hr_data['left']
+
+# Normalization is required for NN
+min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1, 1))
+np_scaled = min_max_scaler.fit_transform(X)
+X = pd.DataFrame(np_scaled, columns=X.columns)
+
+
+# Set validation parameters
 sample_size = int(0.7 * hr_data.shape[0])
 sample_seed = 0
 
-X = hr_data.drop('left', axis=1)
-y = hr_data['left']
-
+# Separate training and validation
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=sample_size, random_state=sample_seed)
 
-net = mx.symbol.Variable('data')
-net = mx.symbol.FullyConnected(net, name='fc1', num_hidden=3)
-net = mx.symbol.Activation(net, name='sig1', act_type="sigmoid")
-net = mx.symbol.FullyConnected(net, name='fc2', num_hidden=2)
-net = mx.symbol.Activation(net, name='sig2', act_type="sigmoid")
-mlp = mx.symbol.SoftmaxOutput(net, name='softmax', multi_output=True)
 
-nd_iter = mx.io.NDArrayIter(data={'data':X_train},
-                            label={'softmax_label':y_train},
+# Build neural net
+data = mx.symbol.Variable('data')
+fc1 = mx.symbol.FullyConnected(data, name='fc1', num_hidden=3)
+act1 = mx.symbol.Activation(fc1, name='sig1', act_type="sigmoid")
+fc2 = mx.symbol.FullyConnected(act1, name='fc2', num_hidden=2)
+act2 = mx.symbol.Activation(fc2, name='sig2', act_type="sigmoid")
+fc3 = mx.symbol.FullyConnected(act2, name='fc3', num_hidden=2)
+mlp = mx.symbol.SoftmaxOutput(fc3, name='softmax')
+
+mx.viz.plot_network(mlp)
+
+
+# Train the net
+import logging
+logging.basicConfig(level=logging.INFO)
+
+mod = mx.mod.Module(symbol=mlp, context=mx.cpu(), data_names=['data'], label_names=['softmax_label'])
+
+batch_size = 5
+learning_rate = 0.3
+
+train_iter = mx.io.NDArrayIter(data={'data':X_train.values},
+                            label={'softmax_label':y_train.values},
                             batch_size=batch_size)
-print(nd_iter.provide_data)
-print(nd_iter.provide_label)
+
+eval_iter = mx.io.NDArrayIter(data={'data':X_test.values},
+                            label={'softmax_label':y_test.values},
+                            batch_size=batch_size)
+
+mod.fit(train_iter,
+        optimizer='sgd',
+        optimizer_params={'learning_rate':learning_rate},
+        eval_metric='accuracy',
+        num_epoch=10)
 
 
+y_pred = mod.predict(eval_iter).asnumpy()
+y_pred = map(lambda x: 1 if x[0] < 0.5 else 0, y_pred)
 
-mlpmodel <- mx.mlp(data = train.preds
-                   ,label = train.target
-                   ,hidden_node = c(3,2) #two layers- 1st Layer with 3 nodes and 2nd with 2 nodes
-                   ,out_node = 2 #Number of output nodes
-                   ,activation="sigmoid" #activation function for hidden layers
-                   ,out_activation = "softmax")
+confusion_matrix(y_test, y_pred)
 
-                   ,num.round = 10 #number of iterations
-                   ,array.batch.size = 5 #Batch size for updating weights
-                   ,learning.rate = 0.03 #same as step size
-                   ,eval.metric= mx.metric.accuracy
-                   ,eval.data = list(data = test.preds, label = test.target))
-
+# Calculo métricas de evaluación
+score = mod.score(eval_iter, ['mse', 'acc'])
+score
